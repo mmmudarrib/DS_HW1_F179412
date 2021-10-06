c733f8315d4f37e1a7a9c6df4df85bf73b3346bf
Mudarrib <mmudarrib2807@gmail.com>
2021-10-06 22:02:49 +0500

    Done till slide 1-10

--- DS Home_work.R
+++ DS Home_work.R
@@ -13,3 +13,14 @@ tabAll <- read.table("foo.txt",
                      colClasses = classes)
 classes
 tabAll
+y <- data.frame(a = 1, b = "a")
+y
+dput(y)
+structure(list(a = 1,
+               b = structure(1L, .Label = "a",
+                             class = "factor")),
+          .Names = c("a", "b"), row.names = c(NA, -1L),
+          class = "data.frame")
+ dput(y, file = "y.R")
+ new.y <- dget("y.R")
+ new.y 
